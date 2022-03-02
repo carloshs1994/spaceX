eda37f48bbf38336a17a68bd86b2d991e4e64299
Carlos Herver <carlos.h.s.1994@gmail.com>
2022-03-02 16:56:26 -0600

    Add logic to links, we now know which is active

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,31 @@ const NavBar = () => (
   <div className="d-flex flex-row justify-content-between border-bottom">
     <div className="mx-3">I am NavBar</div>
     <div>
-      <NavLink to="/" activeClassName="active-link "><span className="mx-3">Rockets</span></NavLink>
-      <NavLink to="/Missions" activeClassName="active-link "><span className="mx-3">Missions</span></NavLink>
-      <NavLink to="/MyProfile" activeClassName="active-link "><span className="mx-3">My Profile</span></NavLink>
-
+      <NavLink
+        to="/"
+        style={({ isActive }) => (isActive
+          ? { textDecoration: 'underline' }
+          : { textDecoration: 'none' })}
+      >
+        <span className="mx-3">Rockets</span>
+      </NavLink>
+      <NavLink
+        to="/Missions"
+        style={({ isActive }) => (isActive
+          ? { textDecoration: 'underline' }
+          : { textDecoration: 'none' })}
+      >
+        <span className="mx-3">Missions</span>
+      </NavLink>
+      <span>|</span>
+      <NavLink
+        to="/MyProfile"
+        style={({ isActive }) => (isActive
+          ? { textDecoration: 'underline' }
+          : { textDecoration: 'none' })}
+      >
+        <span className="mx-3">My Profile</span>
+      </NavLink>
     </div>
   </div>
 );
